445532115534e95b219e2b1c9a3a266bc08b7591
sovalke <sonja.valkeinen@gmail.com>
2019-02-03 18:45:16 +0200

    Lisätty tietokanta Herokulle

--- application/__init__.py
+++ application/__init__.py
@@ -2,14 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ktietokanta.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ktietokanta.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
 # Varsinaisen ohjelman tiedot
 from application import views
-
 from application.breeds import models
 from application.breeds import views
 from application.auth import models 
@@ -31,4 +36,7 @@ login_manager.login_message = "Kirjaudu sisään, jotta voit käyttää tätä t
 def load_user(user_id):
     return User.query.get(user_id)
   
-db.create_all()
+try: 
+    db.create_all()
+except:
+    pass
