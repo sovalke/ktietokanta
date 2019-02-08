544590a51f41dbedf0caf509e4fb4479189ab007
sovalke <sonja.valkeinen@gmail.com>
2019-02-08 20:44:11 +0200

    Bootstrap otettu käyttöön

--- application/breeds/models.py
+++ application/breeds/models.py
@@ -1,5 +1,7 @@
 from application import db
 from application.models import Base
+from sqlalchemy.sql import text
+
 
 class Rotu(Base):
     __tablename__ = "rotu"
@@ -11,3 +13,15 @@ class Rotu(Base):
     def __init__(self, nimi, linja):
         self.nimi = nimi
         self.linja = linja
+
+
+@staticmethod
+def listaa_rodut():
+    stmt = text("SELECT Rotu.id, Rotu.nimi FROM Rotu")
+    res = db.engine.execute(stmt)
+
+    response = []
+    for row in res:
+        response.append({"id": row[0], "nimi": row[1]})
+
+    return response
