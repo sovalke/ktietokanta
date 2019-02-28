ed919f7ff59ece40336e9a1879c164c06f75046c
sovalke <sonja.valkeinen@gmail.com>
2019-02-28 19:41:07 +0200

    Lisätty koodiin kommentteja

--- application/litters/views.py
+++ application/litters/views.py
@@ -14,6 +14,7 @@ from application.litters.forms import LitterForm, PupForm
 
 from datetime import datetime, date
 
+# Uuden pentueen lisääminen
 @app.route("/pentueet/lisaa/")
 @login_required()
 def pentue_lomake():
@@ -24,6 +25,7 @@ def pentue_lomake():
 
     return render_template("litters/lisaapentue.html", form = form)
 
+# Uuden pentueen lisääminen
 @app.route("/pentueet/lisaa/", methods=["POST"])
 @login_required()
 def pentue_lisaa():
@@ -35,18 +37,15 @@ def pentue_lisaa():
     if not form.validate():
         return render_template("litters/lisaapentue.html", form = form)
 
-    print( request.form )
-
     pvm = datetime.strptime(request.form.get("syntynyt"), '%d.%m.%Y').date()
+    lisattava = Pentue(request.form.get("nimi"), pvm, request.form.get("kasvattaja"), request.form.get("isa"), request.form.get("ema"))
 
-    t = Pentue(request.form.get("nimi"), pvm, request.form.get("kasvattaja"), request.form.get("isa"), request.form.get("ema"))
-
-    db.session().add(t)
+    db.session().add(lisattava)
     db.session().commit()
   
     return redirect(url_for("pentue_index"))
 
-# Vain kirjautunut käyttäjä voi muokata pentueita
+# Pentueen muokkaus
 @app.route("/pentueet/<pentue>/muokkaa/", methods=["POST"])
 @login_required()
 def pentue_muokkaa(pentue):
@@ -54,25 +53,25 @@ def pentue_muokkaa(pentue):
     form.kasvattaja.choices = [(g.id, g.nimi) for g in User.query.order_by('nimi')]
     form.isa.choices = [(g.id, g.nimi) for g in Elain.query.order_by('nimi')]
     form.ema.choices = [(g.id, g.nimi) for g in Elain.query.order_by('nimi')]
-    t = Pentue.query.get(pentue)
+    muokattava = Pentue.query.get(pentue)
 
-    t.nimi = request.form.get("nimi")
-    t.syntynyt = request.form.get("syntynyt")
-    t.kasvattaja = request.form.get("kasvattaja")
-    t.isa = request.form.get("isa")
-    t.ema = request.form.get("ema")
+    muokattava.nimi = request.form.get("nimi")
+    muokattava.syntynyt = request.form.get("syntynyt")
+    muokattava.kasvattaja = request.form.get("kasvattaja")
+    muokattava.isa = request.form.get("isa")
+    muokattava.ema = request.form.get("ema")
     pvm = datetime.strptime(request.form.get("syntynyt"), '%d.%m.%Y').date()
 
-    t.syntynyt = pvm
+    muokattava.syntynyt = pvm
 
     if not form.validate():
-        return render_template("litters/pentue_muokkaus.html", pentue = t, form = form)
+        return render_template("litters/pentue_muokkaus.html", pentue = muokattava, form = form)
 
     db.session().commit()
   
-    return redirect(url_for("pentue_index"))
+    return redirect(url_for("pentue_yksi", pentue = muokattava.id))
 
-# Vain kirjautunut käyttäjä voi muokata pentueita.
+# Pentueen muokkaaminen
 @app.route("/pentueet/<pentue>/muokkaa/", methods=["GET"])
 @login_required()
 def pentue_muokkaa_yksi(pentue):
@@ -80,28 +79,30 @@ def pentue_muokkaa_yksi(pentue):
     form.kasvattaja.choices = [(g.id, g.nimi) for g in User.query.order_by('nimi')]
     form.isa.choices = [(g.id, g.nimi) for g in Elain.query.order_by('nimi')]
     form.ema.choices = [(g.id, g.nimi) for g in Elain.query.order_by('nimi')]
-
-    t = Pentue.query.get(pentue)
+    muokattava = Pentue.query.get(pentue)
 
     if request.method == 'GET':
-        form.nimi.data = t.nimi
-        form.syntynyt.data = t.syntynyt
-        form.kasvattaja.data = t.kasvattaja
-        form.isa.data = t.isa
-        form.ema.data = t.ema
+        form.nimi.data = muokattava.nimi
+        form.syntynyt.data = muokattava.syntynyt
+        form.kasvattaja.data = muokattava.kasvattaja
+        form.isa.data = muokattava.isa
+        form.ema.data = muokattava.ema
 
     if not form.validate():
-        return render_template("litters/pentue_muokkaus.html", form = form, pentue = t)
+        return render_template("litters/pentue_muokkaus.html", form = form, pentue = muokattava)
 
-    return render_template("litters/pentue_muokkaus.html", pentue=t, form = form)
+    return render_template("litters/pentue_muokkaus.html", pentue=muokattava, form = form)
 
+# Pennun lisääminen
 @app.route("/pentueet/<pentue>/lisaapentu/")
 @login_required()
 def pentu_lomake(pentue):
     form = PupForm()
     form.pentu.choices = [(g.id, g.nimi) for g in Elain.query.order_by('nimi')]
-    return render_template("litters/lisaapentu.html", pentue = Pentue.query.get(pentue), form = form)
+    kasiteltavaPentue = Pentue.query.get(pentue)
+    return render_template("litters/lisaapentu.html", pentue = kasiteltavaPentue, form = form)
 
+# Pennun lisääminen
 @app.route("/pentueet/<pentue>/lisaapentu/", methods=["POST"])
 @login_required()
 def pentu_lisaa(pentue):
@@ -119,7 +120,7 @@ def pentu_lisaa(pentue):
   
     return redirect(url_for("pentue_yksi", pentue = pentue.id))
 
-# Vain ylläpitäjä voi poistaa pentueita.
+# Pentueen poistaminen
 @app.route("/pentueet/poista<pentue_id>/", methods=["POST"])
 @login_required(role="ADMIN")
 def pentue_poista(pentue_id):
@@ -129,13 +130,14 @@ def pentue_poista(pentue_id):
   
     return redirect(url_for("pentue_index"))
 
+# Pentuelistaus
 @app.route("/pentueet", methods=["GET"])
 def pentue_index():
     stmt = text("SELECT Pentue.id, Pentue.nimi, Pentue.syntynyt, Kasvattaja.id as kasvattaja_id, Kasvattaja.nimi AS kasvattaja_nimi FROM Pentue, Kasvattaja WHERE Pentue.kasvattaja = Kasvattaja.id")
     res = db.engine.execute(stmt)
     return render_template("litters/pentuelista.html", pentueet = res)
 
-
+# Yksittäisen pentueen tarkastelu
 @app.route("/pentueet/<pentue>/", methods=["GET"])
 def pentue_yksi(pentue):
     pentue = Pentue.query.get(pentue)
