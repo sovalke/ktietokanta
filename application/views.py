cbce26a8a1988b8f67ecd0064d5d4788c4d88ab3
sovalke <sonja.valkeinen@gmail.com>
2019-02-28 20:58:31 +0200

    Lisätty tilastonäkymä etusivulle

--- application/views.py
+++ application/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
-from application import app
+from application import app, db
+from sqlalchemy.sql import text
+from application.models import Base
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", tilasto = Base.tilasto())
